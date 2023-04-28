696d1bf67e5d855fc6bf3130d0849902d849dfb8
김명진 <107060000+KMJ1348@users.noreply.github.com>
2023-04-29 01:46:11 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int x = 0;
+        int y = 0;
+        
+        
+        for(int i = 0; i<num_list.length; i++){
+           if(num_list[i] %2 == 0) {
+            x++;
+        } else {
+            y++;
+        }
+    }
+        int[] answer = {x,y};
+        
+        return answer;
+    }
+}
