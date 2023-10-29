ca81673205799f7408dab3c784cdde266cfb12f8
김명진 <107060000+KMJ1348@users.noreply.github.com>
2023-10-29 10:08:58 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -2,10 +2,11 @@ class Solution {
     public long solution(long n) {
         long answer = 0;
         
-        double sqrt = Math.sqrt(n);
-        
+        double sqrt = Math.sqrt(n);  //Math 클래스의 sqrt() 함수는 제곱근을 구할 수 있다.
+                                    //sqrt(x) x 에 제곱근을 구하고 싶은 수를 넣는다.
         if(sqrt % 1 == 0){
-            answer = (long) Math.pow(sqrt + 1,2);
+            answer = (long) Math.pow(sqrt+1,2); //Math 클래스의 pow() 함수는 제곱을 구할 수 있다.
+                                                //pow(x,y) x를 y만큼 곱해준다.
         } else answer = -1;
         
         return answer;
